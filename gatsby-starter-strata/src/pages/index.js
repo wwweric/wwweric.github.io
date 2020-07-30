96633709002579c356891cb474ec449bdfb8a99c
mkn nkm <oyasumi120@gmail.com>
2020-07-30 23:15:04 +0900

    add sns link

--- gatsby-starter-strata/src/pages/index.js
+++ gatsby-starter-strata/src/pages/index.js
@@ -19,18 +19,18 @@ const HomeIndex = () => {
         <section id="one">
           <header className="major">
             <h2>
-              Ipsum lorem dolor aliquam ante commodo
+              Midori Nakamura
               <br />
-              magna sed accumsan arcu neque.
+              Frontend/Markup engneer
             </h2>
           </header>
           <p>
-            Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc
-            nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae
-            lobortis tortor primis integer massa adipiscing id nisi accumsan
-            pellentesque commodo blandit enim arcu non at amet id arcu magna.
-            Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate
-            lorem neque cubilia.
+            2013/4~2016/4 株式会社モンスター・ラボ<br />
+            2016/10~ 自営業<br />
+            <br />
+            HTML/CSS/javascript(jquery)/sass/Pug(Jade)/gulp<br />
+            Wordpress/Bootstrap/Photoshop/Illustrator<br />
+            Git/Terminal  etc...<br />
           </p>
         </section>
 
@@ -44,69 +44,21 @@ const HomeIndex = () => {
         <section id="three">
           <h2>Get In Touch</h2>
           <p>
-            Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
-            magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-            vulputate lorem neque lorem ipsum dolor.
           </p>
           <div className="row">
-            <div className="8u 12u$(small)">
-              <form method="post" action="#">
-                <div className="row uniform 50%">
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="12u">
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <ul className="actions" style={{ marginTop: 30 }}>
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </form>
-            </div>
             <div className="4u 12u$(small)">
               <ul className="labeled-icons">
                 <li>
-                  <h3 className="icon fa-home">
-                    <span className="label">Address</span>
+                  <h3 className="icon fa-twitter">
+                    <span className="label">Twitter</span>
                   </h3>
-                  1234 Somewhere Rd.
-                  <br />
-                  Nashville, TN 00000
-                  <br />
-                  United States
+                  <a href="https://twitter.com/wwweric">wwweric</a>
                 </li>
                 <li>
-                  <h3 className="icon fa-mobile">
-                    <span className="label">Phone</span>
+                  <h3 className="icon fa-facebook">
+                    <span className="label">Facebook</span>
                   </h3>
-                  000-000-0000
-                </li>
-                <li>
-                  <h3 className="icon fa-envelope-o">
-                    <span className="label">Email</span>
-                  </h3>
-                  <a href="#">hello@untitled.tld</a>
+                  <a href="https://m.me/nkmr.mmm">nkmr.mmm</a>
                 </li>
               </ul>
             </div>
