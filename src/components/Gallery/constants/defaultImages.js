51e61256a65c3baaf6f933a060b9a0f73f84900d
mkn nkm <oyasumi120@gmail.com>
2020-09-01 22:46:54 +0900

    add text

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -1,15 +1,9 @@
 import full01 from '../../../assets/images/fulls/kasahara.png'
 import full02 from '../../../assets/images/fulls/ttpro.png'
 import full03 from '../../../assets/images/fulls/asougi.png'
-import full04 from '../../../assets/images/fulls/04.jpg'
-import full05 from '../../../assets/images/fulls/05.jpg'
-import full06 from '../../../assets/images/fulls/06.jpg'
 import thumb01 from '../../../assets/images/thumbs/kasahara.png'
 import thumb02 from '../../../assets/images/thumbs/ttpro.png'
 import thumb03 from '../../../assets/images/thumbs/asougi.png'
-import thumb04 from '../../../assets/images/thumbs/04.jpg'
-import thumb05 from '../../../assets/images/thumbs/05.jpg'
-import thumb06 from '../../../assets/images/thumbs/06.jpg'
 
 
 export const DEFAULT_IMAGES = [
@@ -19,7 +13,7 @@ export const DEFAULT_IMAGES = [
       thumbnail: thumb01,
       links: "https://kappo-kasahara.com",
       caption: '割烹かさ原',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      description: '三軒茶屋の割烹かさ原さまのサイトのマークアップを行いました。PCとSPのみ対応しております。使用スキル：sass、pug、gulp、bootstrapなど',
     },
     {
       id: '2',
@@ -27,7 +21,7 @@ export const DEFAULT_IMAGES = [
       thumbnail: thumb02,
       links: "https://tech.timeticket.jp/",
       caption: 'Time Ticket Pro',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      description: 'Time Ticket Proさまのデザインリニューアルの際に参加させていただきました。使用スキル：Ruby on Rails、html、css、sass、ERB、javascript、jqueryなど',
     },
     {
       id: '3',
@@ -35,6 +29,6 @@ export const DEFAULT_IMAGES = [
       thumbnail: thumb03,
       links: "http://asougi.me/",
       caption: 'asougi',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      description: '小山内さやかさまのポートフォリオサイトのマークアップを行いました（デザインは小山内さま担当）。『さわって楽しい見て気持ちいい』をテーマにされているため、動きの部分の実装には細かい打ち合わせを重ねました。使用スキル：sass、pug、gulp、bootstrapなど',
     },
 ]
