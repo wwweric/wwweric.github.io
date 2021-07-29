4e94bb0c51ab74b291324cb7d404d00bc4e81c5e
mkn nkm <oyasumi120@gmail.com>
2021-07-29 19:14:30 +0900

    add new work

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -1,9 +1,11 @@
 import full01 from '../../../assets/images/fulls/kasahara.png'
 import full02 from '../../../assets/images/fulls/ttpro.png'
 import full03 from '../../../assets/images/fulls/asougi.png'
+import full04 from '../../../assets/images/fulls/cheer-sta.jpg'
 import thumb01 from '../../../assets/images/thumbs/kasahara.png'
 import thumb02 from '../../../assets/images/thumbs/ttpro.png'
 import thumb03 from '../../../assets/images/thumbs/asougi.png'
+import thumb04 from '../../../assets/images/thumbs/cheer-sta.jpg'
 
 
 export const DEFAULT_IMAGES = [
@@ -16,12 +18,20 @@ export const DEFAULT_IMAGES = [
     //   description: '三軒茶屋の割烹かさ原さまのサイトのマークアップを行いました。PCとSPのみ対応しております。使用スキル：sass、pug、gulp、bootstrapなど',
     // },
     {
+      id: '4',
+      source: full04,
+      thumbnail: thumb04,
+      links: "https://cheersta.jp/",
+      caption: 'チアスタ!',
+      description: 'CSS設計〜マークアップを担当させていただきました。使用スキルなど：React、less、ant designなど',
+    },
+    {
       id: '2',
       source: full02,
       thumbnail: thumb02,
       links: "https://tech.timeticket.jp/",
       caption: 'Time Ticket Pro',
-      description: 'Time Ticket Proさまのデザインリニューアルの際に参加させていただきました。使用スキル：Ruby on Rails、html、css、sass、ERB、javascript、jqueryなど',
+      description: 'Time Ticket Proさまのデザインリニューアルの際に参加させていただきました。使用スキルなど：Ruby on Rails、html、css、sass、ERB、javascript、jqueryなど',
     },
     {
       id: '3',
@@ -29,6 +39,6 @@ export const DEFAULT_IMAGES = [
       thumbnail: thumb03,
       links: "http://asougi.me/",
       caption: 'asougi',
-      description: '小山内さやかさまのポートフォリオサイトのマークアップを行いました（デザインは小山内さま担当）。『さわって楽しい見て気持ちいい』をテーマにされているため、動きの部分の実装には細かい打ち合わせを重ねました。使用スキル：sass、pug、gulp、bootstrapなど',
+      description: '小山内さやかさまのポートフォリオサイトのマークアップを行いました（デザインは小山内さま担当）。『さわって楽しい見て気持ちいい』をテーマにされているため、動きの部分の実装には細かい打ち合わせを重ねました。使用スキルなど：sass、pug、gulp、bootstrapなど',
     },
 ]
