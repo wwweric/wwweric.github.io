9881cffd967cc02e46defb8165315b358eb9f8f0
mkn nkm <oyasumi120@gmail.com>
2022-04-21 18:31:40 +0900

    実績修正

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -1,44 +1,24 @@
-import full01 from '../../../assets/images/fulls/kasahara.png'
-import full02 from '../../../assets/images/fulls/ttpro.png'
-import full03 from '../../../assets/images/fulls/asougi.png'
-import full04 from '../../../assets/images/fulls/cheer-sta.jpg'
-import thumb01 from '../../../assets/images/thumbs/kasahara.png'
-import thumb02 from '../../../assets/images/thumbs/ttpro.png'
-import thumb03 from '../../../assets/images/thumbs/asougi.png'
-import thumb04 from '../../../assets/images/thumbs/cheer-sta.jpg'
+import full_cheer from '../../../assets/images/fulls/cheer-sta.jpg'
+import full_fit from '../../../assets/images/fulls/fitstats.jpg'
+import thumb_cheer from '../../../assets/images/thumbs/cheer-sta.jpg'
+import thumb_fit from '../../../assets/images/thumbs/fitstats.jpg'
 
 
 export const DEFAULT_IMAGES = [
-    // {
-    //   id: '1',
-    //   source: full01,
-    //   thumbnail: thumb01,
-    //   links: "https://kappo-kasahara.com",
-    //   caption: '割烹かさ原',
-    //   description: '三軒茶屋の割烹かさ原さまのサイトのマークアップを行いました。PCとSPのみ対応しております。使用スキル：sass、pug、gulp、bootstrapなど',
-    // },
-    {
-      id: '4',
-      source: full04,
-      thumbnail: thumb04,
-      links: "https://cheersta.jp/",
-      caption: 'チアスタ!',
-      description: 'CSS設計〜マークアップを担当させていただきました。使用スキルなど：React、less、ant designなど',
-    },
-    {
-      id: '2',
-      source: full02,
-      thumbnail: thumb02,
-      links: "https://tech.timeticket.jp/",
-      caption: 'Time Ticket Pro',
-      description: 'Time Ticket Proさまのデザインリニューアルの際に参加させていただきました。使用スキルなど：Ruby on Rails、html、css、sass、ERB、javascript、jqueryなど',
-    },
-    {
-      id: '3',
-      source: full03,
-      thumbnail: thumb03,
-      links: "http://asougi.me/",
-      caption: 'asougi',
-      description: '小山内さやかさまのポートフォリオサイトのマークアップを行いました（デザインは小山内さま担当）。『さわって楽しい見て気持ちいい』をテーマにされているため、動きの部分の実装には細かい打ち合わせを重ねました。使用スキルなど：sass、pug、gulp、bootstrapなど',
-    },
+  {
+    id: '1',
+    source: full_cheer,
+    thumbnail: thumb_cheer,
+    links: "https://cheersta.jp/",
+    caption: 'チアスタ!',
+    description: 'CSS設計〜マークアップを担当させていただきました。使用技術など：React、less、ant designなど',
+  },
+  {
+    id: '2',
+    source: full_fit,
+    thumbnail: thumb_fit,
+    links: "https://fitstats.jp/",
+    caption: 'FitStats',
+    description: 'FitStatsさまのサイトの実装をさせていただきました。使用技術など：pug、sass、javascript、jquery、gulp、Bootstrapなど',
+  },
 ]
